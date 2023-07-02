06994a4cbbe442602f7b707a5fe61e2395f8a88d
Subhajit Chatterjee <Subhajit.chatterjee88@gmail.com>
2023-07-02 13:16:40 +0530

    first commit

---
+++ src/test/java/com/java/springservicegateway/SpringServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.springservicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServiceGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
